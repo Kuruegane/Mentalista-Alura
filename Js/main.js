fb9f389593b205add817e031d6ca99023d05a296
Gabriel Freitas <71287983+Kuruegane@users.noreply.github.com>
2021-03-27 20:32:12 -0300

    new files

---
+++ Js/main.js
@@ -0,0 +1,31 @@
+var n = parseInt(Math.random() * 10);
+var t = 3;
+
+while (t > 0) {
+  
+  var k = parseInt(prompt("Digite um Número entre 0 e 10"));
+
+
+  if (n == k) {
+    Swal.fire(
+    'Parabéns!',
+    'Você Acertou!',
+    'success', 
+  )
+    break
+  }
+    else if (k > n) {
+      alert("O Número Secreto é Menor!")}
+    else if (k < n) {
+      alert("O Número Secreto é Maior!")}
+
+    t = t - 1;
+}
+
+if (k != n) {
+ Swal.fire(
+  'Essa Não!',
+  'Suas Tentativas Acabaram! O Número Era: ' + n,
+  'error', 
+)
+}
